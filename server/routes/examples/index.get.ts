17ead99444a0f9947b96fdcbb85a4d5cf8c3ea5d
Maximilian von Beck <maximilianvb07@gmail.com>
2024-12-23 22:39:51 +0100

    example questions

--- server/routes/examples/index.get.ts
+++ server/routes/examples/index.get.ts
@@ -14,7 +14,7 @@ export default eventHandler(async (event) => {
             throw new Error("Failed to Load example Questions");
         }
 
-        return  Response.json({"success": true, questions: result.value});
+        return  Response.json({"success": true, questions: result});
     } catch (error) {
         console.error("Error loading questions from datatabase:", error);
         return Response.json({"error": error.message}, {status: 500});
