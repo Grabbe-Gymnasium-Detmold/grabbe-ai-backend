a8b47d04128af76524f495152d155b321f4dcb1c
Maximilian von Beck <maximilianvb07@gmail.com>
2024-12-23 22:21:51 +0100

    example questions

---
+++ server/routes/examples/index.get.ts
@@ -0,0 +1,22 @@
+import executeQuery from "../../../lib/db";
+
+
+export default eventHandler(async (event) => {
+
+    try {
+        // Schreibe die Sitzung in die Datenbank
+        const result = await executeQuery({
+            query: `SELECT value FROM settings WHERE setting = ?`,
+            values: ["frontend.exampleQuestions"],
+        });
+
+        if (result.error) {
+            throw new Error("Failed to Load example Questions");
+        }
+
+        return  Response.json({"success": true, questions: result.data.value});
+    } catch (error) {
+        console.error("Error loading questions from datatabase:", error);
+        return Response.json({"error": error.message}, {status: 500});
+    }
+});
