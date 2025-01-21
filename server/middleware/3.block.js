3b0196c3570cee06d650d5e00d3a863fef1db1aa
Gameplayer_1_8 <maximilianvb07@gmail.com>
2025-01-21 21:38:09 +0100

    Change Http code to work

--- server/middleware/3.block.js
+++ server/middleware/3.block.js
@@ -2,13 +2,10 @@ import {checkDatacenter, checkVPN} from "~~/lib/blockIPs";
 
 export default defineEventHandler(async (event) => {
     const ip = getRequestHeader(event, "x-forwarded-for") || event.node.req.socket.remoteAddress
-    if(await checkVPN(ip) || await checkDatacenter(ip)){
-        setResponseStatus(event, 451); // Kein Inhalt
-        return Response.json({"status":"error", "message": "Nice try, but we don´t allow VPNS!"}); // Beende die Verarbeitung für OPTIONS
+    if(await checkVPN(ip) || await checkDatacenter(ip)) {
+        return Response.json({"status": "error", "message": "Nice try, but we don´t allow VPNS!"}, {status: 451});
+
     }
-
-
-
 
 
 });
